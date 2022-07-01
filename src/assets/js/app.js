d3811e28ca2f3ad3d7247afe6dc1f841c8c4886b
KonstantinVolodko <volodko2000@bk.ru>
2022-07-01 18:36:24 +0300

    commit12

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,42 +1,3 @@
-// const modal = document.getElementById("myModal");
-// const btn = document.getElementById('myBtn')
-// const span2 = document.getElementsByClassName("close")[0];
-
-
-
-// btn.onclick = function () {
-//     modal.style.display = "block";
-// }
-
-// span.onclick = function() {
-//     modal.style.display = "none"
-// }
-
-// window.onclick = function(event) {
-//     if(event.target == modal) {
-//         modal.style.display = "none"
-//     }
-// }
-
-// const modal2 = document.getElementById("myModal2");
-// const btn2 = document.getElementById('myBtn2')
-// const span = document.getElementsByClassName("close")[0];
-
-
-
-// btn2.onclick = function () {
-//     modal2.style.display = "block";
-// }
-
-// span.onclick = function() {
-//     modal2.style.display = "none"
-// }
-
-// window.onclick = function(event) {
-//     if(event.target == modal2) {
-//         modal2.style.display = "none"
-//     }
-// }
 
 //= components/
 
@@ -45,20 +6,21 @@
 const swiper = new Swiper('.swiper', {
     // Optional parameters
     loop: true,
-  
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-    },
-  
     // Navigation arrows
     navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
+      nextEl: '.next',
+      prevEl: '.prev',
     },
   
-    // And if we need scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
+  });
+
+
+  const swiper2 = new Swiper(".servicesSwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    loop: true,
+    navigation: {
+      nextEl: ".nextBtnServices",
+      prevEl: ".prevBtnServices",
     },
   });
